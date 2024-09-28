a0bac343a05c8aad19b1bc6ec6b9c20afc920343
RBxRobson <robsonnbarbosa0@gmail.com>
2024-09-28 14:45:21 -0300

    feat: Adiciona factory do model Hero

---
+++ home/factories/__init__.py
@@ -0,0 +1,1 @@
+from .hero_factory import HeroFactory
---
+++ home/factories/hero_factory.py
@@ -0,0 +1,23 @@
+import factory
+from ..models import Hero
+
+class HeroFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Hero
+
+    id = factory.Faker('random_int', min=1, max=1000)
+    
+    # Gera um link para uma imagem aleatória
+    image_url = factory.Faker('image_url')  
+    
+    # Gera uma frase aleatória como título
+    title = factory.Faker('sentence', nb_words=4)  
+    
+    # Gera uma frase aleatória como subtítulo
+    subtitle = factory.Faker('sentence', nb_words=6)  
+    
+    # Gera um parágrafo como descrição
+    description = factory.Faker('paragraph')  
+    
+    # Palavra aleatória destacada
+    highlighted_text = factory.Faker('word')  
