1552b0cc2d051240848be126c289afc10d6b3a59
RBxRobson <robsonnbarbosa0@gmail.com>
2024-10-21 10:50:06 -0300

    feat: Adiciona rota do app products ao arquivo central de rotas

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("home/", include("home.urls")),
     path("icons/", include("icons.urls")),
-    path("categories/", include("categories.urls"))
+    path("categories/", include("categories.urls")),
+    path("products/", include("products.urls")),
 ]
